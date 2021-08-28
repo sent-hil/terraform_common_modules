704fed5d4ee6870fc0bc63ec38a1d8363f104aed
Senthil Arivudainambi <me@sent-hil.com>
2021-08-27 17:08:23 -0700

    name the vpc

--- vpc/main.tf
+++ vpc/main.tf
@@ -17,6 +17,10 @@ resource "aws_vpc" "vpc" {
   cidr_block           = "10.0.0.0/16" # 65,536 IP addresses
   enable_dns_hostnames = true
   enable_dns_support   = true
+
+  tags = {
+    Name = var.project
+  }
 }
 
 # -----------------------------------------------------------------------------
